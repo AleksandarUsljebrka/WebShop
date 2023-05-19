9516583aa07afcace3e99a40d8dba853c16e2899
AleksandarUsljebrka <usljebrkaaleksandar@gmail.com>
2023-05-19 17:23:37 +0200

    Added empty components for Login and Registration

--- web-shop/src/App.js
+++ web-shop/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Register from './components/reg-log/Register';
+import Login from './components/reg-log/Login';
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+       
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ web-shop/src/components/reg-log/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Register = () =>{
+    return(
+        <div>
+            <h1>REGISTRATION PAGE</h1>
+        </div>
+    )
+}
+export default Register;
